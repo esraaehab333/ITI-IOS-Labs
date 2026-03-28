04f5c3aa5b57cb62dbe4f3556547d22b05e13e02
esraaehab333 <esraaehabb333@gmail.com>
2026-03-28 12:13:13 -0700

    lab1_2 appdelegate life cycle done

---
+++ lab1/lab1_2(AppDelegate)/lab1_2(AppDelegate)/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  lab1_2(AppDelegate)
+//
+//  Created by ZATER on 3/28/26.
+//  Copyright © 2026 ZATER. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
